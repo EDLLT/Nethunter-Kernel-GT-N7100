0c16b8cb04d4df7636144ae8c30371c248800f55
EDLLT <47005435+EDLLT@users.noreply.github.com>
2023-04-08 00:20:37 +0300

    Fix add-HID-km-support-samsung___smdk4412___t03g_____3.4.67 patch

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1389,7 +1389,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	}
 	/* HID driver always enabled, it's the whole point of this kernel patch */
 	if (hid_enabled)
-		android_enable_function(dev, "hid");
+		android_enable_function(dev, conf, "hid");
 
 	mutex_unlock(&dev->mutex);
 
