003776f7ac496869e714732d9417761a8b1ef4ff
EDLLT <47005435+EDLLT@users.noreply.github.com>
2023-04-07 18:32:01 +0300

    Applied add-HID-km-support-samsung___smdk4412___t03g_____3.4.67 patch

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -56,6 +56,9 @@
 #include "f_adb.c"
 #include "f_mtp.c"
 #include "f_accessory.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #define USB_ETH_RNDIS y
 #include "f_rndis.c"
 #include "rndis.c"
@@ -1084,6 +1087,41 @@ static struct android_usb_function audio_source_function = {
 	.attributes	= audio_source_function_attributes,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
 	&adb_function,
@@ -1096,6 +1134,7 @@ static struct android_usb_function *supported_functions[] = {
 	&diag_function,
 	&dm_function,
 	&audio_source_function,
+	&hid_function,
 	NULL
 };
 
@@ -1269,6 +1308,7 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	char buf[256], *b;
 	char aliases[256], *a;
 	int err;
+	int hid_enabled;
 	int is_ffs;
 	int ffs_enabled = 0;
 
@@ -1336,6 +1376,8 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 			if (err)
 				pr_err("android_usb: Cannot enable '%s' (%d)",
 								   name, err);
+			if (!strcmp(name, "hid"))
+				hid_enabled = 1;
 		}
 	}
 
@@ -1345,6 +1387,9 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 				  struct android_configuration, list_item);
 		free_android_config(dev, conf);
 	}
+	/* HID driver always enabled, it's the whole point of this kernel patch */
+	if (hid_enabled)
+		android_enable_function(dev, "hid");
 
 	mutex_unlock(&dev->mutex);
 
